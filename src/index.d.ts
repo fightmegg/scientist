7b4b0ae40e0e904b8815be9841fee148132b152c
Kamran Ayub <kamran.ayub2@target.com>
2020-10-30 11:47:55 -0500

    rename: tcleaned

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,10 +1,10 @@
 declare module '@fightmegg/scientist' {
-  class Scientist<TControl = any, TCandidate = TControl, TCleanResult = TControl> {
+  class Scientist<TControl = any, TCandidate = TControl, TCleaned = TControl> {
     constructor(name: string, options?: ScientistOptions);
 
     enabled(enabledFn: () => boolean): void;
-    publish(publishFn: (results: ExperimentResults<TControl, TCandidate, TCleanResult>) => void): void;
-    clean(cleanFn: (value: TControl | TCandidate) => TCleanResult): void;
+    publish(publishFn: (results: ExperimentResults<TControl, TCandidate, TCleaned>) => void): void;
+    clean(cleanFn: (value: TControl | TCandidate) => TCleaned): void;
 
     use(controlFn: () => any): void;
     try(candidateFn: () => any): void;
@@ -22,18 +22,18 @@ declare module '@fightmegg/scientist' {
 
   export type ControlOrCandidate = 'control' | 'candidate';
 
-  export interface ExperimentResults<TControl = any, TCandidate = TControl, TCleanResult = TControl> {
-    control?: ExperimentValue<TControl, TCleanResult>;
-    candidate?: ExperimentValue<TCandidate, TCleanResult>;
+  export interface ExperimentResults<TControl = any, TCandidate = TControl, TCleaned = TControl> {
+    control?: ExperimentValue<TControl, TCleaned>;
+    candidate?: ExperimentValue<TCandidate, TCleaned>;
     execution_order: ControlOrCandidate[];
     error: boolean;
     name: string;
     matched: boolean;
   }
 
-  export interface ExperimentValue<TResult = any, TCleanResult = TResult> {
+  export interface ExperimentValue<TResult = any, TCleaned = TResult> {
     value?: TResult;
-    cleaned_value?: TCleanResult;
+    cleaned_value?: TCleaned;
     error?: Error;
     duration: number;
     start_time: number;
